5eb691d91cd9b3ad6fd716ada8a49ca7a5591fcb
danielmeijer <danielmeijer96@hotmail.com>
2019-01-10 16:43:35 +0100

    cleaning array

--- examples.php
+++ examples.php
@@ -1,19 +1,27 @@
 <?php
 
 // JSON Chatfuel to send Text
-$data = array('messages'=> array(array('text'=>'Este texto ha sido generado desde este script')));
+$data = array('messages'=> array(array('text'=>'Este texto ha sido generado desde internet')));
 
 // JSON Chatfuel to send image
-$data = array('messages'=> array(array('attachment'=> array('type'=>'image','payload'=> array('url'=>'https://www.google.es/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png')))));
+
+$url='https://www.google.es/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png';
+$data = array('messages'=> array(array('attachment'=> array('type'=>'image','payload'=> array('url'=>$url)))));
 
 // JSON Chatfuel to send video
-$data = array('messages'=> array(array('attachment'=> array('type'=>'video','payload'=> array('url'=>'https://rockets.chatfuel.com/assets/video.mp4')))));
+
+$url='https://rockets.chatfuel.com/assets/video.mp4';
+$data = array('messages'=> array(array('attachment'=> array('type'=>'video','payload'=> array('url'=>$url)))));
 
 // JSON Chatfuel to send audio
-$data = array('messages'=> array(array('attachment'=> array('type'=>'audio','payload'=> array('url'=>'https://rockets.chatfuel.com/assets/hello.mp3')))));
+
+$url='https://rockets.chatfuel.com/assets/hello.mp3';
+$data = array('messages'=> array(array('attachment'=> array('type'=>'audio','payload'=> array('url'=>$url)))));
 
 // JSON Chatfuel to send file
-$data = array('messages'=> array(array('attachment'=> array('type'=>'file','payload'=> array('url'=>'https://rockets.chatfuel.com/assets/ticket.pdf')))));
+
+$url='https://rockets.chatfuel.com/assets/ticket.pdf';
+$data = array('messages'=> array(array('attachment'=> array('type'=>'file','payload'=> array('url'=>$url)))));
 
 echo json_encode($data);
 
